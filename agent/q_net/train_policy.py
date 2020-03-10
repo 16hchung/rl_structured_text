3c1ea0f5dfe722d0c2ccc30bd2c33bfe308de281
Ubuntu <nlp@qlearn-nlp.4brscp21ywoexdqbxtgs3mh3oa.bx.internal.cloudapp.net>
2020-03-10 09:33:00 +0000

    adjustments to train_policy for gpt as policy

--- agent/q_net/train_policy.py
+++ agent/q_net/train_policy.py
@@ -1,5 +1,6 @@
 from transformers import GPT2LMHeadModel, GPT2Tokenizer, GPT2Config, BertForNextSentencePrediction, BertTokenizer
-from utils import rand_gen_first_token
+from utils import rand_gen_first_token, prompt
+from copy import deepcopy
 import torch
 import torch.nn
 import torch.distributions
@@ -13,10 +14,11 @@ import argparse
 from sequential import bert_seq
 parser = argparse.ArgumentParser()
 parser.add_argument('--gpt_as_policy', action='store_true')
-parser.add_argument('--r_prob_scaler', default=1.)
+parser.add_argument('--r_prob_scaler', default=6.)
 parser.add_argument('--r_tgt_word_scaler', default=1.)
 parser.add_argument('--r_simscore_scaler', default=.5)
 parser.add_argument('--r_seq_scaler', default=.5)
+parser.add_argument('--r_no_repeat_scaler', default=5.)
 cmd_args = parser.parse_args()
 
 # CONSTANTS
@@ -33,12 +35,12 @@ STATE_SZ = 768
 BATCH_SZ = 100
 N_BATCHES = 5 
 MAX_PATH = 1e4
-SAVEPATH = 'PG_combined_r_network.bin'
+SAVEPATH = 'GPTasPG_combined_r_network.bin' if cmd_args.gpt_as_policy else 'PG_combined_r_network.bin'
 gamma = .99
 learning_rate = .01
-fname = 'PG_combined_r.txt'
-MAX_LENGTH = 994 #1024
-FILEPATH = 'PG_combined_r'
+fname = 'GPTasPG_combined_r.txt' if cmd_args.gpt_as_policy else 'PG_combined_r.txt'
+MAX_LENGTH = 993 #1024
+FILEPATH = 'GPTasPG_combined_r' if cmd_args.gpt_as_policy else 'PG_combined_r'
 paths = []
 
 #device = torch.device("cuda" if args.cuda else "cpu")
@@ -57,8 +59,13 @@ bert_model = BertForNextSentencePrediction.from_pretrained('bert-base-cased')
 bert_tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
 
 ''' POLICY TRAINING CODE '''
-if args.gpt_as_policy:
-    policy = GPT2LMHeadModel.from_pretrained('gpt2', config = config)
+if cmd_args.gpt_as_policy:
+    policy = model
+    model_lm_state_dict = deepcopy(model.lm_head.state_dict())
+    for param in policy.transformer.parameters():
+        param.requires_grad = False
+    for param in policy.lm_head.parameters():
+        param.requires_grad = True
 else:
     policy = Policy()
 policy.to(device)
@@ -66,8 +73,8 @@ policy.cuda()
 
 #policy.model.load_state_dict(torch.load(SAVEPATH))
 
-if args.gpt_as_policy:
-    optimizer = torch.optim.RMSprop(policy.lm_head.parameters(), lr=learning_rate)
+if cmd_args.gpt_as_policy:
+    optimizer = torch.optim.AdamW(policy.lm_head.parameters(), lr=.0001)
 else:
     optimizer = torch.optim.RMSprop(policy.parameters(), lr=learning_rate)
 
@@ -104,7 +111,15 @@ def gen_episode(outf, epi, f, epoch, data):
         path['action'] = []
         path['reward'] = []
         policy.eval() # freeze
+        if cmd_args.gpt_as_policy:
+            pol_lm_state_dict = deepcopy(model.lm_head.state_dict())
+            model.lm_head.load_state_dict(model_lm_state_dict)
         generated, past, init_state = rand_gen_first_token(model, tokenizer, device=device) #None)
+        if cmd_args.gpt_as_policy:
+            policy.lm_head.load_state_dict(pol_lm_state_dict)
+            prompt_encode = tokenizer.encode(prompt) 
+            ctxt = torch.tensor([prompt_encode]).cuda()
+            _, action_past, _ = policy(ctxt, past=None)
         context = torch.tensor([generated], device=device)
         sequence = tokenizer.decode(generated)
         f['epoch' + str(epoch)]['eps' + str(epi)]['state'][0] = tokenizer.decode(generated[-1])
@@ -112,23 +127,32 @@ def gen_episode(outf, epi, f, epoch, data):
         length = 0
         prev_sent = None
         cur_sent = [generated[0]]
+        last_token = 0
         # generate trajectory for episode
         while generated[-1] != tokenizer.encode([END_TOKEN])[0] and length < MAX_LENGTH-1:
-            logits, past, hiddens = model(context, past=past)
             #logits = output[0]
             #past = output[1]
             #hiddens = output[2] 
+            if cmd_args.gpt_as_policy:
+                pol_lm_state_dict = deepcopy(model.lm_head.state_dict())
+                model.lm_head.load_state_dict(model_lm_state_dict)
+            logits, past, hiddens = model(context, past=past)
+            if cmd_args.gpt_as_policy:
+                policy.lm_head.load_state_dict(pol_lm_state_dict)
             if len(hiddens[-1].shape) > 2:
                 state = hiddens[-1][:,-1,:]
             else:
                 state = hiddens[-1]
-            if args.gpt_as_policy:
-                logits, _,_ = policy(state)
-                probs = torch.softmax(logits, -1)
-                m = torch.distributions.Categorical(probs)
+            if cmd_args.gpt_as_policy:
+                # sample action from policy net output
+                action_logits, action_past, _ = policy(context, past=action_past)
+                action_probs = torch.softmax(action_logits, -1).squeeze(0).squeeze(0)
+                m = torch.distributions.Categorical(action_probs)
                 action = m.sample().item()
+                # get reward from model net
+                probs = torch.softmax(logits, -1).squeeze(0).squeeze(0)
                 r_prob = probs[action].item()
-                token = action
+                token = torch.tensor(action, device=device)
             else:
                 action_logits = policy(state)
                 probs, idx = torch.topk(logits[...,-1,:], k=5, dim=-1)
@@ -143,20 +167,23 @@ def gen_episode(outf, epi, f, epoch, data):
             length += 1
             context = token.unsqueeze(0)
             sequence = tokenizer.decode(generated)
+            r_no_repeat = -1. if token.item() == last_token else 0.
+            last_token = token.item()
             r_tgt = 1. if token in target_words else 0.
             r_simscore = math.sqrt(abs((init_state[0] @ state[0]).item()))/768.0
             sim_reward = cmd_args.r_simscore_scaler * r_simscore
+            no_repeat_reward = cmd_args.r_no_repeat_scaler * r_no_repeat
             tgt_reward = cmd_args.r_tgt_word_scaler * r_tgt
             prob_reward = cmd_args.r_prob_scaler * r_prob
             r_seq = 0.0
             if tokenizer.decode(generated[-1]) in SENT_END_TOKENS:
-                cur_sent = tokenizer.decode(cur_sent)
+                cur_sent = tokenizer.decode(cur_sent[:511])[:511]
                 if prev_sent is not None:
                     r_seq = bert_seq(device, bert_model, bert_tokenizer, prev_sent, cur_sent)
-                prev_sent = cur_sent
+                prev_sent = cur_sent[:511]
                 cur_sent = []
             seq_reward = cmd_args.r_seq_scaler * r_seq
-            reward = prob_reward + tgt_reward + sim_reward + seq_reward
+            reward = prob_reward + tgt_reward + sim_reward + seq_reward + no_repeat_reward
             f['epoch' + str(epoch)]['eps' + str(epi)]['state'][length+1] = tokenizer.decode(generated[-1])
             f['epoch' + str(epoch)]['eps' + str(epi)]['emb_state'][length+1] = state.cpu()
             f['epoch' + str(epoch)]['eps' + str(epi)]['scaled_reward_prob'][length] = prob_reward
@@ -165,26 +192,31 @@ def gen_episode(outf, epi, f, epoch, data):
             f['epoch' + str(epoch)]['eps' + str(epi)]['combined_reward'][length] = reward
             f['epoch' + str(epoch)]['eps' + str(epi)]['prob'][length] = r_prob
             f['epoch' + str(epoch)]['eps' + str(epi)]['action'][length] = action
-            path['state'].append(state)
+            if not cmd_args.gpt_as_policy:
+                path['state'].append(state)
             path['action'].append(action)
             path['reward'].append(np.array(reward))
         outf.write(sequence)
+        if cmd_args.gpt_as_policy:
+            path['state'] = generated
         f['epoch' + str(epoch)]['eps' + str(epi)]['final_length'][0] = length 
         f['epoch' + str(epoch)]['eps' + str(epi)]['final_reward'][0] = reward_sum
         if length < MIN_LENGTH:
-            path['reward'][-1] -= 200
-            f['epoch' + str(epoch)]['eps' + str(epi)]['final_reward'][0] -= 200
-            f['epoch' + str(epoch)]['eps' + str(epi)]['combined_reward'][-1] -= 200
+            path['reward'][-1] -= 1000
+            f['epoch' + str(epoch)]['eps' + str(epi)]['final_reward'][0] -= 1000
+            f['epoch' + str(epoch)]['eps' + str(epi)]['combined_reward'][-1] -= 1000
         if length >= MAX_LENGTH:
             outf.write(END_TOKEN)
             path['reward'][-1] -= 100
             f['epoch' + str(epoch)]['eps' + str(epi)]['final_reward'][0] -= 100
             f['epoch' + str(epoch)]['eps' + str(epi)]['combined_reward'][-1] -= 100
         paths.append(path)
+        print('ep done')
 
 losses = []
 length_arr = []
 rewards_arr = []
+max_cum_loss = 0
 with open(fname, 'w') as outf:
     f = h5py.File(FILEPATH + '.hdf5', 'w')
     for epoch in range(N_EPOCH):
@@ -198,12 +230,17 @@ with open(fname, 'w') as outf:
         # train the network after every epoch (= 5 eps per epoch)
         for path in paths:
             optimizer.zero_grad()
-            states = path['state']
             actions = path['action']
             rewards = path['reward']
-            states = torch.cat(states).cuda()
             rewards = np.array(rewards)
-            action_logits = policy(states)
+            if cmd_args.gpt_as_policy:
+                prompt_encode = tokenizer.encode(prompt) 
+                ctxt = torch.tensor([prompt_encode + path['state']]).cuda()
+                action_logits = policy(ctxt, past=None)[0][0,len(prompt_encode)+1:,:]
+            else:
+                states = path['state']
+                states = torch.cat(states).cuda()
+                action_logits = policy(states)
             returns, cum_rewards, sent_length = get_returns(rewards)
             rewards_arr.append(cum_rewards)
             length_arr.append(sent_length)
@@ -212,21 +249,29 @@ with open(fname, 'w') as outf:
             cum_loss += loss.item()
             loss.backward()
             optimizer.step()
+        if cmd_args.gpt_as_policy:
+            last_sent = tokenizer.decode(paths[-1]['state'])
         paths = []
         if epoch % LOG_FREQ == 0:
-            torch.save(policy.model.state_dict(), SAVEPATH)
-            torch.save(optimizer.state_dict(), SAVEPATH + '.optim')
+            if max_cum_loss < cum_loss:
+                torch.save(policy.state_dict() if cmd_args.gpt_as_policy else policy.model.state_dict(), SAVEPATH)
+                torch.save(optimizer.state_dict(), SAVEPATH + '.optim')
+                max_cum_loss = cum_loss
             losses.append(cum_loss/EPIS_PER_EPOCH)
             temp_losses = np.array(losses)
-            np.save('PG_combined_r_training_loss.npy', temp_losses)
+            loss_file = 'GPTasPG_combined_r_training_loss.npy' if cmd_args.gpt_as_policy else 'PG_combined_r_training_loss.npy'
+            np.save(loss_file, temp_losses)
             rewards_np = np.array(rewards_arr)
-            np.save('PG_combined_r_rewards.npy', rewards_np)
+            rewards_file = 'GPTasPG_combined_r_rewards.npy' if cmd_args.gpt_as_policy else 'PG_combined_r_rewards.npy'
+            np.save(rewards_file, rewards_np)
             length_np = np.array(length_arr)
-            np.save('PG_combined_r_length.npy', length_np)
+            length_file = 'PG_combined_r_length.npy' if cmd_args.gpt_as_policy else 'PG_combined_r_length.npy'
+            np.save(length_file, length_np)
+            print(last_sent)
         print('Finished epoch!' + str(epoch))
     f.close()
     print('DONE')
 
 losses = np.array(losses)
-np.save('policy_loss.npy', losses)
+np.save('GPTaspolicy_loss.npy' if cmd_args.gpt_as_policy else 'policy_loss.npy', losses)
 
